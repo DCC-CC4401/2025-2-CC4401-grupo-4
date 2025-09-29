72f0815f5a047b2a3e603d841bdd4bbb1ddb6f86
Jimmy Aguilera <jimmyaguilera010699@gmail.com>
2025-09-29 19:58:35 -0300

    feat(accounts): add user authentication with custom login and signup views

--- uclases/accounts/urls.py
+++ uclases/accounts/urls.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import views
+from django.urls import path
+from .views import signin_view, signup_view, logout_view
 
 app_name = 'accounts'
 
-urlpatterns = []
+urlpatterns = [
+    path('login/', signin_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', signup_view, name='register'),]
--- uclases/accounts/views.py
+++ uclases/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CustomLoginForm , SignUpForm
 # Create your views here.
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Sesion cerrada correctamente.')
+    return redirect('home')
+
+def signin_view(request):
+    if request.method == 'POST':
+        form = CustomLoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f'Bienvenido de nuevo {user.username}!')
+            return redirect('home')
+        else:
+            messages.error(request, 'No pudimos iniciar sesion. Revisa la informacion e intenta de nuevo.')
+    else:
+        form = CustomLoginForm(request)
+    
+    return render(request, 'accounts/login.html', {'form': form})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registro completado, sesion iniciada.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Revisa la informacion y vuelve a intentarlo.')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
