76642c2479c7fd40dd68fd9b49b25c88bf68dc07
errenunezdguionbajo <renato.nunez@ug.uchile.cl>
2025-10-12 20:43:38 -0300

    feat (courses): add edit routes for oferta and solicitud

--- uclases/courses/urls.py
+++ uclases/courses/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('solicitud/<int:pk>/', views.solicitud_detail, name='solicitud_detail'),
     path('oferta/nueva/', views.crear_oferta, name='crear_oferta'),
     path('solicitud/nueva/', views.crear_solicitud, name='crear_solicitud'),
+    path('oferta/<int:pk>/editar/', views.editar_oferta, name='editar_oferta'),
+    path('solicitud/<int:pk>/editar/', views.editar_solicitud, name='editar_solicitud'),
 ]
