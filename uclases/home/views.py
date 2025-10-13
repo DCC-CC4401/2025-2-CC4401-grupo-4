e846b9c0009dfef05657205743e07f6fcc9d0ce1
Jimmy Aguilera <jimmyaguilera010699@gmail.com>
2025-10-12 21:51:22 -0300

    feat(courses): add link to view all publications and limit home publications to five results

--- uclases/home/views.py
+++ uclases/home/views.py
@@ -3,19 +3,22 @@ from courses.models import OfertaClase, SolicitudClase
 from itertools import chain
 from operator import attrgetter
 
+
 def home(request):
-    # Obtener todas las ofertas y solicitudes
-    ofertas = OfertaClase.objects.all()
-    solicitudes = SolicitudClase.objects.all()
-    
-    # Combinar ambas listas y ordenar por fecha de publicación (más recientes primero)
-    publicaciones_recientes = sorted(
-        chain(ofertas, solicitudes),
+   
+    ofertas_qs = OfertaClase.objects.order_by('-fecha_publicacion')
+    solicitudes_qs = SolicitudClase.objects.order_by('-fecha_publicacion')
+
+    publicaciones_recientes_all = sorted(
+        chain(ofertas_qs, solicitudes_qs),
         key=attrgetter('fecha_publicacion'),
-        reverse=True
+        reverse=True,
     )
-    
+
+    publicaciones_recientes = publicaciones_recientes_all[:5]
+
     context = {
         'publicaciones_recientes': publicaciones_recientes,
+        'mostrar_ver_todas': len(publicaciones_recientes_all) > len(publicaciones_recientes),
     }
     return render(request, 'home/home.html', context)
