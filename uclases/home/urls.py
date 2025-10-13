bf186dfc2afaa0e8d1ff08eb0e497bc359cbff98
AdolfoRV <adolfo.rojas.v@ug.uchile.cl>
2025-10-13 03:24:35 -0300

    feat(home): add perfil autocomplete API endpoint to URLs

--- uclases/home/urls.py
+++ uclases/home/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+
+from .views import home, perfil_autocomplete_api
+
+
+app_name = "home"
+
 
 urlpatterns = [
     path("", home, name="home"),
+    path("api/perfiles/", perfil_autocomplete_api, name="perfil-autocomplete-api"),
 ]
