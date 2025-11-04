d9d62ebe9155f54c5a50956ec98e82d377ab760c
Jimmy Aguilera <jimmyaguilera010699@gmail.com>
2025-11-04 18:20:48 -0300

    feat(notifications): implement notifications view to display user notifications

--- uclases/notifications/views.py
+++ uclases/notifications/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def notifications_view(request):
+    # Para mostrar las notificaciones
+    notifications = request.user.notifications.all()
+    context = {
+        'notifications': notifications
+    }
+    return render(request, 'notifications/notifications_detail.html', context)
